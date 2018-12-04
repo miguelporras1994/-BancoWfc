9e3c0b7678ba72370409ff5cb6dfd9b0cc557f26
miguel porras <porraangel@gmail.com>
2018-12-03 22:28:54 -0500

    primera fase de wcf

---
+++ SOAP/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace SOAP
+{
+
+            [DataContract]
+
+
+    public class Cliente
+    {
+        [DataMember]
+        public int id_cliente { get; set; }
+        [DataMember]
+        public string Apellidopaterno { get; set; }
+        [DataMember]
+        public string Apellidomaterno { get; set; }
+        [DataMember]
+        public string Tipodocumento { get; set; }
+        [DataMember]
+        public string Sexo { get; set; }
+        [DataMember]
+        public DateTime Fecha { get; set; }
+        [DataMember]
+        public string Direccion { get; set; }
+        [DataMember]
+        public string Codigopostal { get; set; }
+        [DataMember]
+        public string Estadocivil { get; set; }
+
+
+    }
+}
---
+++ animals.comercial.credito.implementacion/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animals.comercial.credito.implementacion
+{
+    public class Class1
+    {
+    }
+}
